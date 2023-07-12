b72672c2f553933d13360641cc258c620773b016
Kennedymuthuri <muthurikennedy082@gmail.com>
2023-07-12 20:17:09 +0300

    created settings class and base clase

---
+++ CommsWall.WebApp/Models/Connection.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommsWall.WebApp.Models
+{
+    public class Connection
+    {
+        public string LogoPath {set; get;}
+        public string Title {set; get;}
+        public string Text {set; get;}
+    }
+}
---
+++ CommsWall.WebApp/Models/Settings.cs
@@ -0,0 +1,9 @@
+namespace CommsWall.WebApp
+{
+    public class Settings
+    {
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}

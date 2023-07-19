fb6c6f52959a69d3d661f2c30b0fd1ae9b549820
Kennedymuthuri <muthurikennedy082@gmail.com>
2023-07-19 12:50:40 +0300

    created settiongs components

--- CommsWall.WebApp/Models/Settings.cs
+++
@@ -1,9 +0,0 @@
-namespace CommsWall.WebApp
-{
-    public class Settings
-    {
-        public string Title { get; set; }
-        public string SubTitle { get; set; }
-        public string PhotoPath { get; set; }
-    }
-}

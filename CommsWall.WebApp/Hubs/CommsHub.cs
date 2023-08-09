170179c8aeca6dfe82d10f6ea1d238de2a1a2268
benkimz <benkim3619@gmail.com>
2023-08-09 16:44:19 +0300

    CommsHub class

--- CommsWall.WebApp/Hubs/CommsHub.cs
+++ CommsWall.WebApp/Hubs/CommsHub.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using CommsWall.Core.Aggregates.ChatMessageAg;
+using Microsoft.AspNetCore.SignalR;
 
 namespace CommsWall.WebApp.Hubs
 {
     public class CommsHub : Hub
     {
-        //
+        public Task SendMessage(string user, ChatMessage message)
+        {
+            return Clients.Client(user).SendAsync("IncomingMessage", message);
+        }
     }
 }

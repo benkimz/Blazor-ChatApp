8dd725636d8cd652bf36e88759d613478761ac01
benkimz <benkim3619@gmail.com>
2023-08-10 10:28:20 +0300

    First Demo

--- CommsWall.WebApp/Hubs/CommsHub.cs
+++ CommsWall.WebApp/Hubs/CommsHub.cs
@@ -5,9 +5,40 @@ namespace CommsWall.WebApp.Hubs
 {
     public class CommsHub : Hub
     {
-        public Task SendMessage(string user, ChatMessage message)
+        private static Dictionary<string, string> _commsMap = new Dictionary<string, string>();
+
+        public override Task OnConnectedAsync()
         {
-            return Clients.User(user).SendAsync("IncomingMessage", message);
+            var httpContext = Context.GetHttpContext();
+            var userCommsId = httpContext?.Request.Headers["user"].ToString();
+            var commsUserId = Context.UserIdentifier;
+            if (!_commsMap.ContainsKey(userCommsId!))
+            {
+                _commsMap.Add(userCommsId!, Context.UserIdentifier!);
+            }
+            Console.WriteLine($"======> user connecting {userCommsId}");
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            var httpContext = Context.GetHttpContext();
+            var userCommsId = httpContext?.Request.Headers["user"].ToString();
+            if (!_commsMap.ContainsKey(userCommsId!))
+                _commsMap.Remove(userCommsId!);
+            
+            Console.WriteLine($"====> user disconnecting {userCommsId}");
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public Task BroadcastMessage(int userId, string receivedMessage)
+        {
+            if (_commsMap.ContainsKey(userId.ToString()))
+            {
+                return Clients.User(_commsMap[userId.ToString()]).SendAsync("GetBroadcastedMessage", userId, receivedMessage);
+            }
+            Console.WriteLine("==========] Look Out: ");
+            return Task.CompletedTask;
         }
     }
 }
